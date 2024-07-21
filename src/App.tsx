7f34118224727f87709ac7a81eba5e1dea665ac1
hyunyeee <5004ohy@naver.com>
2024-07-21 20:47:07 +0900

    setting: Axios 인스턴스 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import { Axios } from "./api/Axios.ts";
+
 function App() {
-  return <></>;
+  const [foodData, setFoodData] = useState();
+
+  const fetchData = async () => {
+    const response = await Axios.get("/COOKRCP01/json/1/1");
+    setFoodData(response.data.COOKRCP01.row[0].RCP_NM);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [foodData]);
+
+  return <div>{foodData}</div>;
 }
 
 export default App;
