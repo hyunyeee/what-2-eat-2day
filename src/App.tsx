75ad27dea7b2d77ebd979b793d009a7c07f20e2a
hyunyeee <5004ohy@naver.com>
2024-07-22 04:03:53 +0900

    setting: Google Analytics 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RouteChangeTracker from "./components/RouteChangeTracker.tsx";
 import Main from "./pages/Main.tsx";
 import Menus from "./pages/Menus.tsx";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <RouteChangeTracker />
         <Route path="/" element={<Main />} />
         <Route path="/menus" element={<Menus />} />
       </Routes>
